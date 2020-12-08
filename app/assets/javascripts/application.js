930384c37c1b8f3536ec98db6d4933cbc721a2a7
wkn816 <wkn816@gmail.com>
2020-12-08 18:54:24 +0900

    非同期確認

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require jquery_ujs 
 //= require turbolinks
 //= require_tree .
